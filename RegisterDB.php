bd210bd33874e6e6c950c87f7ccf4e7c3eedbd7f
reyfear <38860848+reyfear@users.noreply.github.com>
2018-05-05 01:02:15 +0700

    regis

--- RegisterDB.php
+++ RegisterDB.php
@@ -4,6 +4,24 @@
 	$password = "001122";
 	$dbname = "mdt419";
     $tablename = "lkuserDB";  
-     
+    
+    $email = $_GET["email"];
+    $pass = $_GET["password"];
+    $address = $_GET["address"];
+    $telephone = $_GET["tel"];
+
     $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $sql = "INSERT INTO $tablename (email, password, address, telephone)
+	VALUES ('$email', $pass, '$address', '$telephone')";
+
+	if($conn->query($sql) === TRUE) {
+		echo "New record created successfully";
+    header("Location: http://localhost/MDT419/week12/assignment12/login.html");
+    exit;
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
 ?>
