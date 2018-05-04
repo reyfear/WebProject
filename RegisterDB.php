2d82ddbebbbf51c0caacd5312114194469f2e529
reyfear <38860848+reyfear@users.noreply.github.com>
2018-05-05 00:51:38 +0700

    regis

---
+++ RegisterDB.php
@@ -0,0 +1,9 @@
+<?php
+	$servername = "localhost";
+	$username = "reyfear";
+	$password = "001122";
+	$dbname = "mdt419";
+    $tablename = "lkuserDB";  
+     
+    $conn = new mysqli($servername, $username, $password, $dbname);
+?>
