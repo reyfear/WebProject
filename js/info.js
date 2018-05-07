7cfdebdc9f832c2f80bdf54effd1f807947cfe60
grapescandal <divinetrees@gmail.com>
2018-05-07 16:49:31 +0700

    last

--- js/info.js
+++ js/info.js
@@ -32,3 +32,16 @@ function changePic() {
   var productImg = document.getElementById("productImg");
   productImg.src = this.src;
 }
+
+$(document).ready(function() {
+  $("#cf7_controls").on('click', 'span', function() {
+    $("#cf7 img").removeClass("opaque");
+
+    var newImage = $(this).index();
+
+    $("#cf7 img").eq(newImage).addClass("opaque");
+
+    $("#cf7_controls span").removeClass("selected");
+    $(this).addClass("selected");
+  });
+});
